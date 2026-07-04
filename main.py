ad19cdab5ae69dd13f5e3330ae4b3bbd1ff1c866
Abhinav <devabhi456@gmail.com>
2026-07-04 19:17:44 +0545

    Initialize Expense Tracker project

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Expense Tracker
+Version: 0.1
+
+Author: Abhinav
+"""
+
+print("=" * 40)
+print("      Personal Expense Tracker")
+print("=" * 40)
+
+print("\n1. Add Expense")
+print("2. View Expenses")
+print("3. Exit")
