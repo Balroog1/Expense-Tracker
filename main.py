e14a01dc8b5741e6fcfaf2f3a65a6373205e0173
Abhinav <devabhi456@gmail.com>
2026-07-04 19:57:49 +0545

    Add interactive menu selection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 Expense Tracker
-Version: 0.1
+Version: 0.2
 
 Author: Abhinav
 """
@@ -11,4 +11,22 @@ print("=" * 40)
 
 print("\n1. Add Expense")
 print("2. View Expenses")
-print("3. Exit")
+print("3. Generate Report")
+print("4. Exit")
+
+choice = input("\nEnter your choice: ")
+
+if choice == "1":
+    print("\nYou selected: Add Expense")
+
+elif choice == "2":
+    print("\nYou selected: View Expenses")
+
+elif choice == "3":
+    print("\nYou selected: Generate Report")
+
+elif choice == "4":
+    print("\nThank you for using Expense Tracker!")
+
+else:
+    print("\nInvalid Choice!")
